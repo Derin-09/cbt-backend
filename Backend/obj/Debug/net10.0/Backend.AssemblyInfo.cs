ce72c2693620beff4a1242c98846c4b167ca70f4
Derin <aderinsolasamashimi@gmail.com>
2026-05-04 12:34:34 +0100

    added swagger

--- Backend/obj/Debug/net10.0/Backend.AssemblyInfo.cs
+++ Backend/obj/Debug/net10.0/Backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1b5344948070ee99fc8810918ba0c06880123f55")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4fc5ff996f7e4de448e32b1a433d61161426bcdb")]
 [assembly: System.Reflection.AssemblyProductAttribute("Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
